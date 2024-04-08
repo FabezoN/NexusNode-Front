226c0f1bf0a8e0ef40d6532abb6d81d3aa092781
FabezoN <dela.noe@hotmail.fr>
2024-04-08 15:50:11 +0200

    Mise en place du routage et création homepage

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,10 @@
+function HomePage(){
+    return(
+        <div>
+            <h1>Home Page</h1>
+            <h2>Ceci est la page d'acceuil</h2>
+        </div>
+    )
+}
+
+export default HomePage;
