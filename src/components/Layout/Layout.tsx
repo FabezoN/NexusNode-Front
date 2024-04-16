8120a2542a433bda41e9c757cb0908225d3ccf5f
dorianalbertlaik <dorian.albert1@ynov.com>
2024-04-16 09:31:16 +0200

    update Layout Profile

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,29 +1,32 @@
-import React, {ReactNode} from 'react';
-import './Layout.css'
+import React, { useState, useEffect } from 'react';
+import './Layout.css';
 import Logo from '../../assets/LogoNexusNode.png';
-import {Link, NavLink} from "react-router-dom";
-import Paniers from '../../assets/icons8-panier-90.png';
-import {CgProfile} from "react-icons/cg";
-import {FaBasketShopping} from "react-icons/fa6";
+import { Link, NavLink } from "react-router-dom";
+import { CgProfile } from "react-icons/cg";
+import { FaBasketShopping } from "react-icons/fa6";
 
 interface LayoutProps {
-    children: ReactNode;
-}
-if (sessionStorage.getItem('user') !== null) {
-    const storage = sessionStorage.getItem('user');
-    if (storage !== null) { // Vérifie que storage n'est pas null avant de continuer
-        const userObject = JSON.parse(storage); // Maintenant sûr que storage est une chaîne non-nulle
-        const idUser = userObject.info; // Accéder à la propriété id dans l'objet info
-    }
+    children: React.ReactNode;
 }
 
-const Layout: React.FC<LayoutProps> = ({children}) => {
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+    const [isProfileMenuOpen, setIsProfileMenuOpen] = useState(false);
+    const [userRole, setUserRole] = useState<number | null>(null);
+
+    useEffect(() => {
+        const storage = sessionStorage.getItem('user');
+        if (storage) {
+            const userObject = JSON.parse(storage);
+            setUserRole(userObject.info.role);
+        }
+    }, []);
+
     return (
         <div>
             <header>
                 <div className="LogoN">
                     <Link to="/">
-                        <img alt="Logo" className="Logo" src={Logo}></img>
+                        <img alt="Logo" className="Logo" src={Logo} />
                     </Link>
                 </div>
                 <div className="Nom">
@@ -31,15 +34,39 @@ const Layout: React.FC<LayoutProps> = ({children}) => {
                 </div>
                 <div className="Right">
                     <div className="SearchBar">
-                        <input className="InputS" placeholder="Rechercher un produit"></input>
+                        <input className="InputS" placeholder="Rechercher un produit" />
                     </div>
                     <div className="PanierN">
-                        <FaBasketShopping size={40} className="Icons"></FaBasketShopping>
+                        <FaBasketShopping size={40} className="Icons" />
                     </div>
-                    <div className="ProfilN">
-                        <NavLink to="login">
-                            <CgProfile size={40} className="Icons" ></CgProfile>
-                        </NavLink>
+                    <div className="ProfilN"
+                         onMouseEnter={() => userRole && setIsProfileMenuOpen(true)}
+                         onMouseLeave={() => setIsProfileMenuOpen(false)}>
+                        {userRole ? (
+                            <React.Fragment>
+                                <CgProfile size={40} className="Icons" />
+                                {isProfileMenuOpen && (
+                                    <div className="ProfileMenu">
+                                        {userRole === 1 && (
+                                            <React.Fragment>
+                                                <NavLink to="/profile" className="MenuOption">Profil</NavLink>
+                                                <NavLink to="/logout" className="MenuOption">Déconnexion</NavLink>
+                                            </React.Fragment>
+                                        )}
+                                        {userRole === 2 && (
+                                            <React.Fragment>
+                                                <NavLink to="/adminpage" className="MenuOption">Panel</NavLink>
+                                                <NavLink to="/logout" className="MenuOption">Déconnexion</NavLink>
+                                            </React.Fragment>
+                                        )}
+                                    </div>
+                                )}
+                            </React.Fragment>
+                        ) : (
+                            <NavLink to="/login">
+                                <CgProfile size={40} className="Icons" />
+                            </NavLink>
+                        )}
                     </div>
                 </div>
             </header>
